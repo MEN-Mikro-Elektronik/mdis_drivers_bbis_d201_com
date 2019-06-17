675f93ba018f6459c439f48f3bb0ff141c600b15
Marcin Lassota <mlassota@jpembedded.eu>
2019-06-17 13:38:40 +0200

    Replace %FSREV with MENT_XSTR(MAK_REVISION) revision string in source files
    Fix: 'driver revision strings shall indicate submodule revision tags'
    https://github.com/MEN-Mikro-Elektronik/13MD05-90/issues/9

--- DRIVER/COM/ident.c
+++ DRIVER/COM/ident.c
@@ -1,7 +1,7 @@
 /*********************  P r o g r a m  -  M o d u l e ***********************
  *
  *         Name: ident.c
- *      Project: 
+ *      Project:
  *
  *       Author: ds
  *
@@ -11,9 +11,9 @@
  *     Switches: DBG	  	enable debugging
  *
  *---------------------------[ Public Functions ]----------------------------
- *  
+ *
  *  D201_Ident              Return ident string
- *  
+ *
  *
  *---------------------------------------------------------------------------
  * Copyright (c) 1998-2019, MEN Mikro Elektronik GmbH
@@ -36,9 +36,45 @@
 #include <MEN/men_typs.h>
 #include "d201.h"
 
+#ifdef _D201
+#ifndef MAC_BYTESWAP
+	static const char IdentString[]="D201 - D201 " MENT_XSTR(MAK_REVISION);
+#else
+	static const char IdentString[]="D201 - D201 Swapped " MENT_XSTR(MAK_REVISION);
+#endif
+#endif
+#ifdef _C203
+#ifndef MAC_BYTESWAP
+	static const char IdentString[]="C203 - C203 " MENT_XSTR(MAK_REVISION);
+#else
+	static const char IdentString[]="C203 - C203 Swapped " MENT_XSTR(MAK_REVISION);
+#endif
+#endif
+#ifdef _C204
+#ifndef MAC_BYTESWAP
+	static const char IdentString[]="C204 - C204 " MENT_XSTR(MAK_REVISION);
+#else
+	static const char IdentString[]="C204 - C204 Swapped " MENT_XSTR(MAK_REVISION);
+#endif
+#endif
+#ifdef _F201
+#ifndef MAC_BYTESWAP
+	static const char IdentString[]="F201 - F201 " MENT_XSTR(MAK_REVISION);
+#else
+	static const char IdentString[]="F201 - F201 Swapped " MENT_XSTR(MAK_REVISION);
+#endif
+#endif
+#ifdef _F202
+#ifndef MAC_BYTESWAP
+	static const char IdentString[]="F202 - F202 " MENT_XSTR(MAK_REVISION);
+#else
+	static const char IdentString[]="F202 - F202 Swapped " MENT_XSTR(MAK_REVISION);
+#endif
+#endif
+
 /*********************************** D201_Ident *****************************
  *
- *  Description:  Return ident string 
+ *  Description:  Return ident string
  *
  *---------------------------------------------------------------------------
  *  Input......:  -
@@ -47,27 +83,7 @@
  ****************************************************************************/
 char* Ident( void ) /* nodoc */
 {
-	return ( 
-
-#ifdef _D201
-		"D201 - D201"
-#endif
-#ifdef _C203
-		"C203 - C203"
-#endif
-#ifdef _C204
-		"C204 - C204"
-#endif
-#ifdef _F201
-		"F201 - F201"
-#endif
-#ifdef _F202
-		"F202 - F202"
-#endif
-#ifdef MAC_BYTESWAP
-		" Swapped"
-#endif
-		" Base Board Handler: %FSREV COM/DRIVERS/BBIS/d201 1.59 2014-10-30%" );
+	return ( (char*) IdentString );
 }
 
 
